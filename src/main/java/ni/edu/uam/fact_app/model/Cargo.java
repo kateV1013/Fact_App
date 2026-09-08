6254b1c00ac121926f65fd1a7461096f4d2ea6ee
valenk1310@gmail.com <kvurbina@uamv.edu.ni>
2026-09-08 16:22:19 -0600

    Se agrego util, Cargo y Empleado

--- src/main/java/ni/edu/uam/fact_app/model/Cargo.java
+++ src/main/java/ni/edu/uam/fact_app/model/Cargo.java
@@ -1,4 +1,15 @@
 package ni.edu.uam.fact_app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Cargo {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
 }
--- src/main/java/ni/edu/uam/fact_app/model/Empleado.java
+++ src/main/java/ni/edu/uam/fact_app/model/Empleado.java
@@ -1,4 +1,12 @@
 package ni.edu.uam.fact_app.model;
 
+import java.time.LocalDate;
+
 public class Empleado {
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private Cargo cargo;
+    private LocalDate fechaContratacion;
+    private boolean activo;
 }
