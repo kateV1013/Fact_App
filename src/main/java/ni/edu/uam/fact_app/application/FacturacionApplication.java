8d1a287deaa74400ce4021028a3875a0deaa83b0
valenk1310@gmail.com <kvurbina@uamv.edu.ni>
2026-09-10 15:49:33 -0600

    Icon y colores diseño (se estan cambiado)

--- src/main/java/ni/edu/uam/fact_app/application/FacturacionApplication.java
+++ src/main/java/ni/edu/uam/fact_app/application/FacturacionApplication.java
@@ -3,11 +3,15 @@ package ni.edu.uam.fact_app.application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class FacturacionApplication extends Application {
+
+    private static final String ICONO_APP = "/fact_app/icons/app-icon.png";
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -16,6 +20,9 @@ public class FacturacionApplication extends Application {
         );
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Fact App");
+        stage.getIcons().add(new Image(Objects.requireNonNull(
+                FacturacionApplication.class.getResourceAsStream(ICONO_APP)
+        )));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/ni/edu/uam/fact_app/util/SceneManager.java
+++ src/main/java/ni/edu/uam/fact_app/util/SceneManager.java
@@ -2,10 +2,15 @@ package ni.edu.uam.fact_app.util;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.*;
+
 import java.io.IOException;
+import java.util.Objects;
 
 public final class SceneManager {
+    private static final String ICONO_APP = "/fact_app/icons/app-icon.png";
+
     private SceneManager() { }
 
     public static void abrirVentana(String recurso, String titulo) throws IOException {
@@ -14,6 +19,9 @@ public final class SceneManager {
 
         Stage stage = new Stage();
         stage.setTitle(titulo);
+        stage.getIcons().add(new Image(Objects.requireNonNull(
+                SceneManager.class.getResourceAsStream(ICONO_APP)
+        )));
         stage.setScene(new Scene(new FXMLLoader(url).load()));
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.showAndWait();
